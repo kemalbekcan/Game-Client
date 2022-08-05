7f3a2517b0c3148b1f7ea74b57398025d7dc8014
kemalbekcan <kemalbekcan@gmail.com>
2022-08-05 22:54:54 +0300

    Initial

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  }
 }
 
 module.exports = nextConfig
