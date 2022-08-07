70eed4dc7a6cce8f8c29970f294f82341861361e
kemalbekcan <kemalbekcan@gmail.com>
2022-08-07 10:40:28 +0300

    Initial

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
+import React, { Fragment } from "react";
 import Layout from "../components/Layout";
 
 function Home() {
   return (
-    <div>
+    <Fragment>
       <Layout title="Anasayfa" desc="Anasayfa" styleName="rules">
-        <p>sad</p>
+        <div className="card">
+          <div className="rule-list">
+            <h1>Kurallar</h1>
+            <ul>
+              <li>Diğer kullanıcılara karşı saygılı ol.</li>
+              <li>Konulara bilgin dışında ve konu dışında yorum yapma.</li>
+              <li>
+                Konularda ve Yorumlarda Küfür/Hakaret içeren cümleler kurma
+              </li>
+              <li>Konuyu doğru başlıkta açtığından emin ol</li>
+            </ul>
+          </div>
+          <div>
+            <p>Kuralları Kabul Ediyorum</p>
+          </div>
+        </div>
       </Layout>
-    </div>
+    </Fragment>
   );
 }
 
