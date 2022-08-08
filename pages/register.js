641c881f00107b99dd27f23b3a940c7463f45ed0
kemalbekcan <kemalbekcan@gmail.com>
2022-08-08 23:24:46 +0300

    Register created page

--- pages/register.js
+++ pages/register.js
@@ -1,20 +1,21 @@
 import React, { Fragment } from "react";
 import Layout from "../components/Layout";
 
-function login() {
+function register() {
   function onSubmit(e) {
     e.preventDefault();
     console.log("e", e);
   }
   return (
     <Fragment>
-      <Layout title="Login" desc="login" styleName="login">
+      <Layout title="Register" desc="register" styleName="register">
         <div className="">
           <button className="sign-button">KAYIT OL</button>
           <button className="sign-button">GİRİŞ YAP</button>
         </div>
         <div>
           <form onSubmit={onSubmit}>
+            <input className="primary-input" type="text" name="username" />
             <input className="primary-input" type="text" name="email" />
             <input className="primary-input" type="password" name="password" />
             <input className="primary-button" type="submit" value="GİRİŞ YAP" />
@@ -25,4 +26,4 @@ function login() {
   );
 }
 
-export default login;
+export default register;
