49eae0d95a18971f1409f9f118641b3247e3d8d8
kemalbekcan <kemalbekcan@gmail.com>
2022-08-12 00:46:21 +0300

    Initial

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const path = require("path");
 const nextConfig = {
   trailingSlash: true,
   exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
   ) {
     return {
       '/': { page: '/' },
