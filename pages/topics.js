25a5ad160eec1093218dd49cf6775f7b7cc6fe5f
kemalbekcan <kemalbekcan@gmail.com>
2022-08-14 18:10:12 +0300

    Konular page created

--- pages/topics.js
+++ pages/topics.js
@@ -1,28 +1,49 @@
-import React, { Fragment } from "react"
+import React, { Fragment } from "react";
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 function topics() {
-    return (
-        <Fragment>
-            <Layout title="Konular" desc="konular" styleName="topics">
-                <div>
-                <h1>Genel Konular</h1>
-                <button>KONU AÇ</button>
-                </div>
-                <div className="matters">
-                    <div className="">
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </Layout>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Layout title="Konular" desc="konular" styleName="topics">
+        <div className="matters-head">
+          <h1 className="title">
+            <span className="underline">Genel</span> Konular
+          </h1>
+          <button className="primary-button">KONU AÇ</button>
+        </div>
+        <div className="matters">
+          <div className="matters-card">
+            <div className="matters-icon">
+              <Image
+                src="/assets/bilgisayar-etiketli-konular.png"
+                alt="Bilgisayar Etiketli Konular"
+                color="red"
+                width="294"
+                height="294"
+                objectFit="cover"
+              />
+            </div>
+            <div>Bilgisayar Etiketli Konular</div>
+          </div>
+          <div className="konular">
+            <div className="konu">
+              <p className="text-white title">Konular</p>
+              <p className="text-white">123</p>
+            </div>
+            <div className="konu">
+              <p className="text-white title">Mesajlar</p>
+              <p className="text-white">321</p>
+            </div>
+            <div className="konu">
+              <p className="text-white title">Onaylanmış Yazılar</p>
+              <p className="text-white">231</p>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </Fragment>
+  );
 }
 
-export default topics
+export default topics;
