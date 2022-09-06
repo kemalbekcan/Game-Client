b782fcb4cb70ba1de3a31c29f31d27a982d20690
kemalbekcan <kemalbekcan@gmail.com>
2022-09-06 20:53:17 +0300

    Initial

--- next.config.js
+++ next.config.js
@@ -3,31 +3,9 @@ const path = require("path");
 
 const nextConfig = {
   trailingSlash: true,
-  exportPathMap: async function (
-  ) {
-    return {
-      '/': { page: '/' },
-      '/login': { page: '/login' },
-      '/register': { page: '/register' },
-      '/topics': { page: '/topics' },
-    }
-  },
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-
-    return config;
-  },
   sassOptions: {
     includePaths: [ path.join( __dirname, 'sass' ) ],
   },
-  /**
-   * We specify which domains are allowed to be optimized.
-   * This is needed to ensure that external urls can't be abused.
-   * @see https://nextjs.org/docs/basic-features/image-optimization#domains
-   */
   async headers() {
     return [
       {
